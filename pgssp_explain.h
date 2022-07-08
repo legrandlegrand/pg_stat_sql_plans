0ed28b780a05b8790b9d0b6a6588a683f5ede964
legrandlegrand <legrandlegrand@users.noreply.github.com>
2022-07-08 22:11:18 +0200

    Delete pgssp_explain.h

--- pgssp_explain.h
+++
@@ -1,6 +0,0 @@
-void
-pgssp_ExplainOnePlan(PlannedStmt *plannedstmt, IntoClause *into, ExplainState *es,
-			   const char *queryString, ParamListInfo params,
-			   QueryEnvironment *queryEnv, const instr_time *planduration,
-			   const BufferUsage *bufusage);
-
